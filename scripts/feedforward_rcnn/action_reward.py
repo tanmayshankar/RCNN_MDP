5c49991f2c11322f1cec2e69b5d85fd2025830a0
tanmayshankar <tanmay.shankar@gmail.com>
2016-03-20 23:56:39 +0530

    Updating action reward.

--- scripts/feedforward_rcnn/action_reward.py
+++ scripts/feedforward_rcnn/action_reward.py
@@ -62,6 +62,7 @@ trans_mat = npy.loadtxt(str(sys.argv[2]))
 trans_mat = trans_mat.reshape((action_size,transition_space,transition_space))
 
 action_reward_function = npy.zeros((action_size,discrete_size,discrete_size))
+action_value_layers = npy.zeros(shape=(action_size,discrete_size,discrete_size))
 
 def modify_trans_mat():
 	global trans_mat
@@ -127,15 +128,23 @@ print "\nHere's the reward.\n"
 for i in range(0,discrete_size):
 	print reward_function[i]
 
+def action_reward_bias():
+	global action_reward_function, action_value_layers
+
+	for act in range(0,action_size):
+		action_value_layers[act] += action_reward_function[act]
+
 def conv_layer():	
-	global value_function
-	global trans_mat
-	action_value_layers = npy.zeros(shape=(action_size,discrete_size,discrete_size))
+	global value_function, trans_mat, action_value_layers
+	
 	layer_value = npy.zeros(shape=(discrete_size,discrete_size))
 	for act in range(0,action_size):		
 		#Convolve with each transition matrix.
 		action_value_layers[act]=signal.convolve2d(value_function,trans_mat[act],'same','fill',0)
 	
+	#Fixed bias for reward. 
+	action_reward_bias()
+
 	#Max pooling over actions. 
 	value_function = gamma*npy.amax(action_value_layers,axis=0)
 	# layer_value = gamma*npy.amax(action_value_layers,axis=0)
@@ -153,7 +162,7 @@ def recurrent_value_iteration():
 	t=0	
 	while (t<time_limit):
 		conv_layer()
-		reward_bias()		
+		# reward_bias()		
 		t+=1
 		print t
 	
