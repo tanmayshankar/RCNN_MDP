b2dcf7f868a1338d64a5f45667a66568679bff42
tanmayshankar <tanmay.shankar@gmail.com>
2016-03-21 11:27:36 +0530

    Updating action rewards.

--- scripts/feedforward_rcnn/action_reward.py
+++ scripts/feedforward_rcnn/action_reward.py
@@ -13,40 +13,27 @@ from matplotlib.pyplot import *
 from scipy import signal
 import copy
 
-
-# import cProfile
-# cProfile.run('foo()')
-
-
 basis_size = 3
 discrete_size = 50
 
 #Action size also determines number of convolutional filters. 
 action_size = 8
-# action_space = [[0,1],[1,0],[0,-1],[-1,0],[1,1],[1,-1],[-1,1],[-1,-1]]
 action_space = [[-1,0],[1,0],[0,-1],[0,1],[-1,-1],[-1,1],[1,-1],[1,1]]
-############# UP, DOWN, LEFT, RIGHT, UPLEFT, UPRIGHT, DOWNLEFT, DOWNRIGHT........
+############# UP, DOWN, LEFT, RIGHT, UPLEFT, UPRIGHT, DOWNLEFT, DOWNRIGHT ##################
 
 #Transition space size determines size of convolutional filters. 
 transition_space = 3
 
-# basis_functions = npy.loadtxt(str(sys.argv[1]))
-# reward_weights = npy.loadtxt(str(sys.argv[2]))
-# basis_functions = basis_functions.reshape((basis_size,discrete_size,discrete_size))
-
-# reward_function = basis_functions[0]*reward_weights[0]+basis_functions[2]*reward_weights[2]+basis_functions[1]*reward_weights[1]
 #Static / instantaneous reward. 
 reward_function = npy.loadtxt(str(sys.argv[1]))
-# reward_function = basis_functions[0,:,:]
-# reward_function /=1000.0
-# 
+
 time_limit = 100
 
-reward_function += npy.amin(reward_function)
-reward_function /= npy.amax(reward_function)
-# reward_function -= npy.amax(reward_function)/5
+dummy_rew = copy.deepcopy(reward_function)
+dummy_rew = abs(dummy_rew)
+reward_function /= npy.amax(dummy_rew)
 
-action_factor_reward = 0.05
+action_factor_reward = 0.1
 
 value_functions = npy.zeros(shape=(time_limit,discrete_size,discrete_size))
 value_function = npy.zeros(shape=(discrete_size,discrete_size))
@@ -78,49 +65,18 @@ def modify_trans_mat():
 def create_action_reward():
 	global reward_function, action_reward_function, action_factor_reward
 
-	for i in range(0,action_size):
+	for i in range(0,action_size/2):
 		action_reward_function[i,:,:] = copy.deepcopy(reward_function) - (i%4)*action_factor_reward * npy.amax(reward_function)
+		# print (i%4)
+
+	for i in range(action_size/2,action_size):
+		action_reward_function[i,:,:] = copy.deepcopy(reward_function) - 1.414*(i%4)*action_factor_reward * npy.amax(reward_function)
 
 def initialize():
 	modify_trans_mat()
 	create_action_reward()
 
 initialize()
-
-def conv_transition_filters():
-	global trans_mat
-	trans_mat_1 = [[0.,0.7,0.],[0.1,0.1,0.1],[0.,0.,0.]]
-	# trans_mat_2 = [[0.7,0.1,0.],[0.1,0.1,0.],[0.,0.,0.]]
-	trans_mat_2 = [[0.55,0.15,0.],[0.15,0.15,0.],[0.,0.,0.]]
-	
-	trans_mat[0] = trans_mat_1
-	trans_mat[1] = npy.rot90(trans_mat_1,2)
-	trans_mat[2] = npy.rot90(trans_mat_1,1)
-	trans_mat[3] = npy.rot90(trans_mat_1,3)
-
-	trans_mat[4] = trans_mat_2
-	trans_mat[5] = npy.rot90(trans_mat_2,3)	
-	trans_mat[7] = npy.rot90(trans_mat_2,2)
-	trans_mat[6] = npy.rot90(trans_mat_2,1)
-
-	# trans_mat[:] = npy.fliplr(trans_mat[:])
-	# trans_mat[:] = npy.flipud(trans_mat[:])
-	
-	for i in range(0,action_size):
-		trans_mat[i] = npy.fliplr(trans_mat[i])
-		trans_mat[i] = npy.flipud(trans_mat[i])
-
-	# trans_mat[1] = trans_mat_1
-	# trans_mat[2] = npy.rot90(trans_mat_1)
-	# trans_mat[0] = npy.rot90(trans_mat_1,3)
-	# trans_mat[3] = npy.rot90(trans_mat_1,2)
-
-	# trans_mat[5] = trans_mat_2
-	# trans_mat[7] = npy.rot90(trans_mat_2)
-	# trans_mat[6] = npy.rot90(trans_mat_2,2)
-	# trans_mat[4] = npy.rot90(trans_mat_2,3)
-
-# conv_transition_filters()
 
 print "Transition Matrices:\n",trans_mat
 
@@ -136,8 +92,7 @@ def action_reward_bias():
 
 def conv_layer():	
 	global value_function, trans_mat, action_value_layers
-	
-	layer_value = npy.zeros(shape=(discrete_size,discrete_size))
+
 	for act in range(0,action_size):		
 		#Convolve with each transition matrix.
 		action_value_layers[act]=signal.convolve2d(value_function,trans_mat[act],'same','fill',0)
@@ -147,10 +102,10 @@ def conv_layer():
 
 	#Max pooling over actions. 
 	value_function = gamma*npy.amax(action_value_layers,axis=0)
-	# layer_value = gamma*npy.amax(action_value_layers,axis=0)
-	print "The next value function.",value_function
 	optimal_policy[:,:] = npy.argmax(action_value_layers,axis=0)
-	# return layer_value
+
+	print "The next value function.",value_function
+	
 
 def reward_bias():
 	global value_function
@@ -162,7 +117,6 @@ def recurrent_value_iteration():
 	t=0	
 	while (t<time_limit):
 		conv_layer()
-		# reward_bias()		
 		t+=1
 		print t
 	
@@ -172,14 +126,9 @@ print "Here's the policy."
 for i in range(0,discrete_size):
 	print optimal_policy[i]
 
-# policy_iteration()
-
-# print "These are the value functions."
-# for t in range(0,time_limit):
-# 	print value_functions[t]
 with file('reward_function.txt','w') as outfile: 
 	outfile.write('#Reward Function.\n')
-	npy.savetxt(outfile,1000*reward_function,fmt='%-7.2f')
+	npy.savetxt(outfile,reward_function,fmt='%-7.2f')
 
 with file('output_policy.txt','w') as outfile: 
 	outfile.write('#Policy.\n')
@@ -187,5 +136,5 @@ with file('output_policy.txt','w') as outfile:
 
 with file('value_function.txt','w') as outfile: 
 	outfile.write('#Value Function.\n')
-	npy.savetxt(outfile,1000*value_function,fmt='%-7.2f')
+	npy.savetxt(outfile,value_function,fmt='%-7.2f')
 
