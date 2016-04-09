9be51ac1944b0cd12a26ec8f32c227d34eb79028
tanmayshankar <tanmay.shankar@gmail.com>
2016-04-09 12:33:51 +0530

    Updating saving policy for boundaries.

--- scripts/feedforward_rcnn/action_reward.py
+++ scripts/feedforward_rcnn/action_reward.py
@@ -40,8 +40,8 @@ value_function = npy.zeros(shape=(discrete_size,discrete_size))
 
 optimal_policy = npy.zeros(shape=(discrete_size,discrete_size))
 
-gamma = 0.95
-# gamma = 0.90
+# gamma = 0.95
+gamma = 0.90
 # gamma = 1.
 
 # trans_mat = npy.zeros(shape=(action_size,transition_space,transition_space))
@@ -49,9 +49,10 @@ trans_mat = npy.loadtxt(str(sys.argv[2]))
 trans_mat = trans_mat.reshape((action_size,transition_space,transition_space))
 
 action_reward_function = npy.zeros((action_size,discrete_size,discrete_size))
-action_value_layers = npy.zeros(shape=(action_size,discrete_size,discrete_size))
+action_value_layers  = npy.zeros(shape=(action_size,discrete_size,discrete_size))
+q_value_layers  = npy.zeros(shape=(action_size,discrete_size,discrete_size))
 
-def modify_trans_mat():
+def modify_trans_mat(): 
 	global trans_mat
 	epsilon = 0.0001
 	for i in range(0,action_size):
@@ -65,12 +66,15 @@ def modify_trans_mat():
 def create_action_reward():
 	global reward_function, action_reward_function, action_factor_reward
 
-	for i in range(0,action_size/2):
-		action_reward_function[i,:,:] = copy.deepcopy(reward_function) - (i%4)*action_factor_reward * npy.amax(reward_function)
-		# print (i%4)
+	for i in range(0,action_size):
+		action_reward_function[i,:,:] = copy.deepcopy(reward_function)
+		
+	# for i in range(0,action_size/2):
+	# 	action_reward_function[i,:,:] = copy.deepcopy(reward_function) - (i%4)*action_factor_reward * npy.amax(reward_function)
+	# 	# print (i%4)
 
-	for i in range(action_size/2,action_size):
-		action_reward_function[i,:,:] = copy.deepcopy(reward_function) - 1.414*(i%4)*action_factor_reward * npy.amax(reward_function)
+	# for i in range(action_size/2,action_size):
+	# 	action_reward_function[i,:,:] = copy.deepcopy(reward_function) - 1.414*(i%4)*action_factor_reward * npy.amax(reward_function)
 
 def initialize():
 	modify_trans_mat()
@@ -88,21 +92,26 @@ def action_reward_bias():
 	global action_reward_function, action_value_layers
 
 	for act in range(0,action_size):
-		action_value_layers[act] += action_reward_function[act]
+		# action_value_layers[act] += action_reward_function[act]
+		q_value_layers[act] += action_reward_function[act]
 
 def conv_layer():	
 	global value_function, trans_mat, action_value_layers
 
 	for act in range(0,action_size):		
 		#Convolve with each transition matrix.
-		action_value_layers[act]=signal.convolve2d(value_function,trans_mat[act],'same','fill',0)
+		# action_value_layers[act]=signal.convolve2d(value_function,trans_mat[act],'same','fill',0)
+		q_value_layers[act]=signal.convolve2d(value_function,trans_mat[act],'same','fill',0)
 	
 	#Fixed bias for reward. 
 	action_reward_bias()
 
 	#Max pooling over actions. 
-	value_function = gamma*npy.amax(action_value_layers,axis=0)
-	optimal_policy[:,:] = npy.argmax(action_value_layers,axis=0)
+	# value_function = gamma*npy.amax(action_value_layers,axis=0)
+	# optimal_policy[:,:] = npy.argmax(action_value_layers,axis=0)
+
+	value_function = gamma*npy.amax(q_value_layers,axis=0)
+	optimal_policy[:,:] = npy.argmax(q_value_layers,axis=0)
 
 	print "The next value function.",value_function
 	
@@ -126,9 +135,23 @@ print "Here's the policy."
 for i in range(0,discrete_size):
 	print optimal_policy[i]
 
+optimal_policy[0,:] = 1
+optimal_policy[49,:] = 0
+optimal_policy[:,0] = 3
+optimal_policy[:,49] = 2
+optimal_policy[0,0] = 7
+optimal_policy[0,49] = 6
+optimal_policy[49,0] = 5
+optimal_policy[49,49] = 4
+
 with file('reward_function.txt','w') as outfile: 
 	outfile.write('#Reward Function.\n')
 	npy.savetxt(outfile,reward_function,fmt='%-7.2f')
+
+with file('action_reward_function.txt','w') as outfile: 
+	for data in action_reward_function:
+		outfile.write('#Action Reward Function.\n')
+		npy.savetxt(outfile,data,fmt='%-7.2f')
 
 with file('output_policy.txt','w') as outfile: 
 	outfile.write('#Policy.\n')
@@ -138,3 +161,7 @@ with file('value_function.txt','w') as outfile:
 	outfile.write('#Value Function.\n')
 	npy.savetxt(outfile,value_function,fmt='%-7.2f')
 
+with file('Q_value_function.txt','w') as outfile: 
+	for data in q_value_layers:
+		outfile.write('#Q Value Function.\n')
+		npy.savetxt(outfile,data,fmt='%-7.2f')
