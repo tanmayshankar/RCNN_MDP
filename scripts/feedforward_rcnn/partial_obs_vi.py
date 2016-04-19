8d9d10d5922d730dcf69bb92ea5f02a8ae24ea01
tanmayshankar <tanmay.shankar@gmail.com>
2016-04-19 15:09:54 +0530

    Updating files.

--- scripts/feedforward_rcnn/partial_obs_vi.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-value_function = npy.zeros((discrete_size,discrete_size))
-
-belief_reward = npy.zeros((action_size,discrete_size,discrete_size))
-
-# for i in range(0,action_size):
-# 	belief_reward[i,:,:] = action_reward[i,:,:]*belief[:,:]
-
