c41b45971c737c312d00f6bc6c4f0f13f622d28d
tanmayshankar <tanmay.shankar@gmail.com>
2016-07-12 21:01:39 +0530

    Correcting experience replay RMSProp with belief_target_actions.

--- scripts/QMDP_IRL/experience_replay.py
+++ scripts/QMDP_IRL/experience_replay.py
@@ -1,87 +1,6 @@
 #!/usr/bin/env python
 import numpy as npy
-import matplotlib.pyplot as plt
-import sys
-from mpl_toolkits.mplot3d import Axes3D
-import random
-from matplotlib.pyplot import *
-from scipy import signal
-import copy
-
-###### DEFINITIONS
-discrete_size = 50
-
-#Action size also determines number of convolutional filters. 
-action_size = 9
-action_space = npy.array([[-1,0],[1,0],[0,-1],[0,1],[-1,-1],[-1,1],[1,-1],[1,1],[0,0]])
-## UP, DOWN, LEFT, RIGHT, UPLEFT, UPRIGHT, DOWNLEFT, DOWNRIGHT, NOTHING.
-
-#Transition space size determines size of convolutional filters. 
-transition_space = 3
-obs_space = 3
-h=obs_space/2
-trajectory_index=0
-length_index=0
-
-npy.set_printoptions(precision=3)
-
-#### DEFINING STATE BELIEF VARIABLES
-to_state_belief = npy.zeros(shape=(discrete_size,discrete_size))
-from_state_belief = npy.zeros(shape=(discrete_size,discrete_size))
-corr_to_state_belief = npy.zeros((discrete_size,discrete_size))
-backprop_belief = npy.zeros((discrete_size,discrete_size))
-
-#### DEFINING EXTENDED STATE BELIEFS 
-w = transition_space/2
-to_state_ext = npy.zeros((discrete_size+2*w,discrete_size+2*w))
-from_state_ext = npy.zeros((discrete_size+2*w,discrete_size+2*w))
-
-#### DEFINING OBSERVATION RELATED VARIABLES
-observation_model = npy.zeros(shape=(obs_space,obs_space))
-observed_state = npy.zeros(2)
-current_pose = npy.zeros(2)
-current_pose = current_pose.astype(int)
-observed_state = observed_state.astype(int)
-
-#### Take required inputs. 
-trans_mat_1 = npy.loadtxt(str(sys.argv[1]))
-trans_mat_1 = trans_mat_1.reshape((action_size-1,transition_space,transition_space))
-trans_mat = npy.zeros((action_size,transition_space,transition_space))
-
-for i in range(0,8):
-	trans_mat[i]=trans_mat_1[i]
-trans_mat[8,1,1]=1.
-print trans_mat
-
-q_value_estimate = npy.ones((action_size,discrete_size,discrete_size))
-reward_estimate = npy.zeros((action_size,discrete_size,discrete_size))
-q_value_layers = npy.zeros((action_size,discrete_size,discrete_size))
-value_function = npy.zeros((discrete_size,discrete_size))
-
-qmdp_values = npy.zeros(action_size)
-qmdp_values_softmax = npy.zeros(action_size)
-
-number_trajectories = 47
-trajectory_length = 30
-
-trajectories = npy.loadtxt(str(sys.argv[2]))
-trajectories = trajectories.reshape((number_trajectories,trajectory_length,2))
-
-observed_trajectories = npy.loadtxt(str(sys.argv[3]))
-observed_trajectories = observed_trajectories.reshape((number_trajectories,trajectory_length,2))
-
-actions_taken = npy.loadtxt(str(sys.argv[4]))
-actions_taken = actions_taken.reshape((number_trajectories,trajectory_length))
-target_actions = npy.zeros(action_size)
-belief_target_actions = npy.zeros(action_size)
-
-time_limit = number_trajectories*trajectory_length
-learning_rate = 0.5
-annealing_rate = (learning_rate/5)/time_limit
-
-
-
-from_belief_vector = npy.zeros((trajectory_length, discrete_size, discrete_size))
+from variables import *
 
 def initialize_state():
 	# global current_pose, from_state_belief, observed_state
@@ -120,7 +39,6 @@ def display_beliefs():
 
 def bayes_obs_fusion():
 	global to_state_belief, current_pose, observation_model, obs_space, observed_state, corr_to_state_belief
-	
 	
 	h = obs_space/2
 	intermediate_belief = npy.zeros((discrete_size+2*h,discrete_size+2*h))
@@ -172,13 +90,6 @@ def calc_softmax():
 	for act in range(0,action_size):
 		qmdp_values_softmax[act] = npy.exp(qmdp_values[act]) / npy.sum(npy.exp(qmdp_values), axis=0)
 
-# def update_QMDP_values():
-# 	global to_state_belief, q_value_estimate, qmdp_values, from_state_belief
-
-# 	for act in range(0,action_size):
-# 		# qmdp_values[act] = npy.sum(q_value_estimate[act]*to_state_belief)
-# 		qmdp_values[act] = npy.sum(q_value_estimate[act]*from_state_belief)
-
 def belief_update_QMDP_values():
 	global to_state_belief, q_value_estimate, qmdp_values, from_state_belief, backprop_belief
 
@@ -186,19 +97,6 @@ def belief_update_QMDP_values():
 		# qmdp_values[act] = npy.sum(q_value_estimate[act]*to_state_belief)
 		# qmdp_values[act] = npy.sum(q_value_estimate[act]*from_state_belief)
 		qmdp_values[act] = npy.sum(q_value_estimate[act]*backprop_belief)
-
-# def reward_backprop():
-# 	global reward_estimate, qmdp_values_softmax, target_actions, from_state_belief
-# 	global time_index
-
-# 	update_QMDP_values()
-# 	calc_softmax()
-
-# 	# alpha = learning_rate - annealing_rate*time_index
-# 	alpha = learning_rate
-
-# 	for act in range(0,action_size):
-# 		reward_estimate[act,:,:] -= alpha * (qmdp_values_softmax[act]-target_actions[act]) * from_state_belief[:,:]
 
 def belief_reward_backprop():
 	global reward_estimate, qmdp_values_softmax, target_actions, from_state_belief
@@ -212,15 +110,8 @@ def belief_reward_backprop():
 	alpha = learning_rate
 
 	for act in range(0,action_size):
-		# reward_estimate[act,:,:] -= alpha * (qmdp_values_softmax[act]-target_actions[act]) * from_state_belief[:,:]
-		# reward_estimate[act,:,:] -= alpha * (qmdp_values_softmax[act]-target_actions[act]) * backprop_belief[:,:]
-		
 		reward_estimate[act,:,:] -= alpha * (qmdp_values_softmax[act]-belief_target_actions[act]) * backprop_belief[:,:]
-
-		# print "Action: ", act, "Thing: ", (qmdp_values_softmax[act]-belief_target_actions[act])
-		# print "Trying:"
-		# for i in range(0,50):
-		# 	print backprop_belief[i]
+		# reward_estimate[act,:,:] -= alpha * (qmdp_values_softmax[act]-target_actions[act]) * backprop_belief[:,:]
 
 def belief_prop(traj_ind,len_ind):
 	construct_from_ext_state()
@@ -324,7 +215,7 @@ def Inverse_Q_Learning():
 			backprop()
 			index_list.remove(length_index)
 
-			print "Index: ", length_index
+			# print "Index: ", length_index
 
 		feedback()
 
@@ -338,54 +229,9 @@ def Inverse_Q_Learning():
 		# # draw()
 		# # show() 
 
-	# for trajectory_index in range(0,number_trajectories):
-	# # for trajectory_index in range(0,25):
-		
-	# 	from_belief_vector = npy.zeros((trajectory_length,discrete_size,discrete_size))
-	# 	index_list = range(0,trajectory_length-1)
-
-	# 	parse_data(trajectory_index,0)
-	# 	initialize_state()
-
-	# 	for length_index in range(0,trajectory_length-1):			
-			
-	# 		if (from_state_belief.sum()>0):
-	# 			master(trajectory_index, length_index)
-	# 			time_index += 1
-	# 			print "Trajectory:", trajectory_index, "Step:", length_index
-	# 		else: 
-	# 			print "WARNING: Belief sum below 0."
-	# 			print "Trajectory: ", trajectory_index, "Step:", length_index
-
-	# 	while index_list:
-	# 		length_index = random.choice(index_list)
-	# 		parse_backprop_index(trajectory_index, length_index)
-	# 		backprop()
-	# 		index_list.remove(length_index)
-
-	# 	feedback()
-
-	# 	# imshow(q_value_estimate[0], interpolation='nearest', origin='lower', extent=[0,50,0,50], aspect='auto')
-	# 	# # plt.show(block=False)
-	# 	# colorbar()
-	# 	# plt.show()
-	# 	# # plt.title('Trajectory Index: %i')
-	# 	# # draw()
-	# 	# # show() 
-
 parse_data(0,0)
 initialize_all()
 Inverse_Q_Learning()
-
-# with file('from_beliefs.txt','w') as outfile:
-# 	for data_slice in from_belief_vector:
-# 		outfile.write('#New Slice:')
-# 		npy.savetxt(outfile,data_slice,fmt='%-7.3f')
-
-# # for i in range(0,action_size):
-# # 	imshow(reward_estimate[i], interpolation='nearest', origin='lower', extent=[0,50,0,50], aspect='auto')
-# # 	colorbar()
-# # 	plt.show()
 
 with file('Q_Value_Estimate.txt','w') as outfile:
 	for data_slice in q_value_estimate:
