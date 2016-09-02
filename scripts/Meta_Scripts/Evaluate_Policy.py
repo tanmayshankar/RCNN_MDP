f3f2f33abf93174e393bfbca59e2bd0e570ce271
tanmayshankar <tanmay.shankar@gmail.com>
2016-09-02 19:42:50 -0400

    Updating the Evaluate policy meta scripts.

--- scripts/Meta_Scripts/Evaluate_Policy.py
+++ scripts/Meta_Scripts/Evaluate_Policy.py
@@ -27,10 +27,28 @@ for i in range(1,26):
 	replan_error[4,i-1] = float(npy.count_nonzero(orig_policy-learnt_policy_out))/ 25
 
 for i in range(1,26):
-	orig_policy = npy.loadtxt("reward_{0}/NEW/output_policy_beta.txt".format(i))
+	orig_policy = npy.loadtxt("reward_{0}/output_policy.txt".format(i))
+	learnt_policy_utkr = npy.loadtxt("reward_{0}/Replan_weighted/output_policy.txt".format(i))
+	replan_error[i-1] =  float(npy.count_nonzero(orig_policy-learnt_policy_utkr))/ 25
 
-	learnt_policy_po = npy.loadtxt("reward_{0}/NEW/output_policy_linear.txt".format(i))
-	replan_error[0,i-1] = float(npy.count_nonzero(orig_policy-learnt_policy_po))/ 25
+def eval_movethings(i):
+	# if (not(os.path.isfile('data/QMDP_Experiments/reward_{0}/Replan/UTKR/value_function_evaluated.txt'))):
+	shutil.move("value_function_evaluated.txt","data/QMDP_Experiments/reward_{0}/orig_policy_value_function_evaluated.txt".format(i))
 
-	learnt_policy_fo = npy.loadtxt("reward_{0}/NEW/output_policy_FO_beta.txt".format(i))
-	replan_error[1,i-1] = float(npy.count_nonzero(orig_policy-learnt_policy_fo))/ 25
+for i in range(1,26):
+	command = "scripts/VI_RCNN/VI_evaluate_policy.py data/QMDP_Experiments/reward_{0}/reward_{0}.txt data/learnt_models/estimated_trans_PO_NEW.txt data/QMDP_Experiments/reward_{0}/output_policy.txt".format(i)
+	subprocess.call(command.split(),shell=False)
+	eval_movethings(i)
+	print "ITERATION:", i
+
+orig_values = npy.zeros((25,50,50))
+learnt_values = npy.zeros((25,50,50))
+diff_values = npy.zeros((25,50,50))
+replan_values = npy.zeros(25)
+
+for i in range(1,26):
+	orig_values[i-1] = npy.loadtxt("reward_{0}/orig_policy_value_function_evaluated.txt".format(i))
+	learnt_values[i-1] = npy.loadtxt("reward_{0}/Replan/UTKR/value_function_evaluated.txt".format(i))
+	diff_values[i-1] = learnt_values[i-1] - orig_values[i-1]
+	replan_values[i-1] = float(diff_values[i-1].sum())/2500
+ 
--- scripts/Meta_Scripts/run_Replan.py
+++ scripts/Meta_Scripts/run_Replan.py
@@ -2,10 +2,14 @@ def movethings(i):
 	shutil.move("output_policy.txt","data/BP_Experiments/Replan/reward_{0}/Weighted/output_policy_linear.txt".format(i))
 	shutil.move("value_function.txt","data/BP_Experiments/Replan/reward_{0}/Weighted/value_function_linear.txt".format(i))
 
+def Replan_movethings(i):
+	shutil.move("output_policy.txt","data/QMDP_Experiments/reward_{0}/Replan/UTKR/output_policy.txt".format(i))
+	shutil.move("value_function.txt","data/QMDP_Experiments/reward_{0}/Replan/UTKR/value_function.txt".format(i))
+
 for i in range(1,26):
-	command = "scripts/VI_RCNN/VI_beta_feedforward.py data/BP_Experiments/Replan/reward_{0}/ORIG/reward_{0}.txt data/learnt_models/estimated_trans_weighted.txt".format(i)
-	subprocess.call(command.split(),shell=False)	
-	movethings(i)	
+	command = "scripts/VI_RCNN/VI_beta_feedforward.py data/QMDP_Experiments/reward_{0}/reward_{0}.txt data/learnt_models/estimated_trans_PO_NEW.txt".format(i)
+	subprocess.call(command.split(),shell=False)
+	Replan_movethings(i)
 
 for i in range(1,26):
     shutil.move("reward_{0}/value_function_po.txt".format(i),"reward_{0}/NEW/value_function_po.txt".format(i))
@@ -28,7 +32,7 @@ for i in range(1,26):
 
 for i in range(1,26):
 	orig_policy = npy.loadtxt("reward_{0}/ORIG/output_policy.txt".format(i))
-	learnt_policy_po = npy.loadtxt("reward_{0}/Weighted/output_policy_linear.txt".format(i))
+	learnt_policy_po = npy.loadtxt("reward_{0}/Weighted/output_policy_weighted.txt".format(i))
 	replan_error[i-1] = float(npy.count_nonzero(orig_policy-learnt_policy_po))/ 25
 	
 
@@ -42,3 +46,36 @@ for i in range(1,26):
 	replan_error_1[0,i-1] = float(npy.count_nonzero(orig_policy-learnt_policy_po))/ 25
 	learnt_policy_fo = npy.loadtxt("reward_{0}/NEW/output_policy_FO_beta.txt".format(i))
 	replan_error_1[1,i-1] = float(npy.count_nonzero(orig_policy-learnt_policy_fo))/ 25
+
+
+
+
+
+
+# URKT
+
+for i in range(1,26):
+	command = "scripts/VI_RCNN/VI_beta_ext_reward.py data/QMDP_Experiments/reward_{0}/IRL_1/Reward_Function_Estimate.txt data/learnt_models/actual_transition.txt".format(i)
+	subprocess.call(command.split(),shell=False)	
+	Replan_movethings(i)
+
+# URUT
+
+for i in range(1,26):
+	command = "scripts/VI_RCNN/VI_beta_ext_reward.py data/QMDP_Experiments/reward_{0}/IRL_1/Reward_Function_Estimate.txt data/learnt_models/estimated_trans_weighted.txt".format(i)
+	subprocess.call(command.split(),shell=False)	
+	Replan_movethings(i)
+
+
+def Replan_movethings(i):
+	shutil.move("output_policy.txt","data/QMDP_Experiments/reward_{0}/Replan_weighted_2/URKT/output_policy.txt".format(i))
+	shutil.move("value_function.txt","data/QMDP_Experiments/reward_{0}/Replan_weighted_2/URKT/value_function.txt".format(i))
+
+
+
+
+
+
+#RUNNING REPLANNING FOR EVALUATION of POLICIES in terms of Expected Reward
+
+def eval_movethings():
